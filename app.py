af7754d9f787d90061ce3c768d5054454b1f4929
DattaVi <126326716+DattaVi@users.noreply.github.com>
2023-10-20 23:42:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import argparse  # Import argparse for command-line argument parsing
+import os
+# Sidebar contents
+with st.sidebar:
+    st.title('ChatPDF App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+    ''')
+    add_vertical_space(5)
+
+
+# Load API key from the command-line argument
+parser = argparse.ArgumentParser()
+parser.add_argument('--api_key', type=str, help='Your OpenAI API key')
+args = parser.parse_args()
+openai_api_key = args.api_key
+
+
+# Load the PDF file and perform chatbot actions
+def main():
+    st.header("Chat with PDF 💬")
+
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text=text)
+
+        # Get the store_name from the PDF file name
+        store_name = pdf.name[:-4]
+
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+        else:
+            embeddings = OpenAIEmbeddings(openai_api_key="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")  # Use the API key
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+
+
+if __name__ == '__main__':
+    main()
